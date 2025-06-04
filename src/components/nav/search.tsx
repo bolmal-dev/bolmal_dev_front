8c332b9f36ed7c05c7d339ee22aab73f0ac109f0
kojesung <js95112345@gmail.com>
2025-06-04 13:50:56 +0900

    fix : 헤더 input 패딩 추가

--- src/components/nav/search.tsx
+++ src/components/nav/search.tsx
@@ -3,7 +3,7 @@ export default function Search() {
         <div className="relative">
             <input
                 type="text"
-                className="w-[27.77vw] h-[3.7vh] rounded-[100px] bg-bg-default focus:outline-none focus:border-primary"
+                className="w-[27.77vw] h-[3.7vh] rounded-[100px] bg-bg-default focus:outline-none focus:border-primary pl-[10px]"
             />
             <button className="absolute right-4 top-1/2 -translate-y-1/2">🔍</button>
         </div>
